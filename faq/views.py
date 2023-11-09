53fcf0f802e549c902489acfb182c2c34712747d
Hamed Mohammadi <hamed@dehongi.com>
2023-11-09 23:38:47 +0330

    faq model

--- faq/views.py
+++ faq/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from django.views.generic import ListView
+
+from .models import FAQ
+
 # Create your views here.
+
+
+class FAQListView(ListView):
+    model = FAQ
+    paginate_by = 10
+    context_object_name = "faqs"
