01ec7ae7b5513d570cd292a1bbf4f3d2bc82bdde
Hamed Mohammadi <hamed@dehongi.com>
2023-11-09 22:33:44 +0330

    apps created

--- pages/views.py
+++ pages/views.py
@@ -6,3 +6,7 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "pages/home.html"
+
+
+class AboutPageView(TemplateView):
+    template_name = "pages/about.html"
