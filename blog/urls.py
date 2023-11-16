9579a89bd8f628fec15a3dc57a33e638dd24efc5
Hamed Mohammadi <hamed@dehongi.com>
2023-11-16 21:12:30 +0330

    bog list

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from .views import PostListView
+from .views import PostListView, PostDetailView
 
 app_name = "blog"
 
 urlpatterns = [
     path("", PostListView.as_view(), name="post_list"),
+    path(
+        "<int:year>/<int:month>/<int:day>/<slug:slug>/",
+        PostDetailView.as_view(),
+        name="post_detail",
+    ),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
@@ -8,4 +8,9 @@ from .models import Post
 
 
 class PostListView(ListView):
+    model = Post
+    context_object_name = "posts"
+
+
+class PostDetailView(DetailView):
     model = Post
