f25cbd8dd8e15e481f791f1734c601c420cec500
Hamed Mohammadi <hamed@dehongi.com>
2023-11-23 14:47:03 +0330

    carousel

--- pages/models.py
+++ pages/models.py
@@ -7,6 +7,7 @@ class Carousel(models.Model):
     title = models.CharField(max_length=256)
     body = models.TextField()
     url = models.URLField()
+    url_text = models.CharField(max_length=30, default="Click")
     image = models.ImageField(upload_to="carousel")
 
     def __str__(self) -> str:
