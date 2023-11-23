fab1cfc4a785e02269514b4b7ffc90b4ec373952
Hamed Mohammadi <hamed@dehongi.com>
2023-11-23 15:14:03 +0330

    home

--- pages/admin.py
+++ pages/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Carousel, Feature
+from .models import Carousel, Feature, Page
 
 # Register your models here.
 
 admin.site.register(Carousel)
 admin.site.register(Feature)
+admin.site.register(Page)
--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,7 @@ from typing import Any
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-from .models import Carousel
+from .models import Carousel, Feature, Page
 
 # Create your views here.
 
@@ -14,6 +14,13 @@ class HomePageView(TemplateView):
         context = super().get_context_data(**kwargs)
         carousels = Carousel.objects.all()
         context["carousels"] = carousels
+
+        features = Feature.objects.all()
+        context["features"] = features
+
+        pages = Page.objects.all()
+        context["pages"] = pages
+
         return context
 
 
