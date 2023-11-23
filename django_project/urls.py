d476ca3353f8d4a6ec70f7a605cd426e72b3273b
Hamed Mohammadi <hamed@dehongi.com>
2023-11-23 13:24:07 +0330

    Carousel model

--- django_project/urls.py
+++ django_project/urls.py
@@ -20,8 +20,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("blog/", include("blog.urls", namespace="blog")),
-    path("", include("pages.urls", namespace="pages")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path("blog/", include("blog.urls", namespace="blog")),
+        path("", include("pages.urls", namespace="pages")),
+    ]
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Carousel(models.Model):
+    title = models.CharField(max_length=256)
+    body = models.TextField()
+    url = models.URLField()
+    image = models.ImageField(upload_to="carousel")
