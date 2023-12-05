482d1918cafb93043db8ad644338c47f1282eec6
Hamed Mohammadi <hamed@dehongi.com>
2023-12-05 19:44:14 +0330

    some changes

--- pages/forms.py
+++ pages/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Page
+from .models import Page, Message
 
 
 class PageForm(forms.ModelForm):
     class Meta:
         model = Page
         fields = ["title", "slug", "intro", "body", "image"]
+
+
+class MessageForm(forms.ModelForm):
+    class Meta:
+        model = Message
+        fields = ["name", "email", "subject", "message"]
--- pages/models.py
+++ pages/models.py
@@ -49,3 +49,17 @@ class Page(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+
+class Message(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.subject
