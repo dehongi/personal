990fd408ded5c54f943233c09555490d80c11356
Hamed Mohammadi <hamed@dehongi.com>
2023-12-05 21:14:30 +0330

    cleaning

--- pages/admin.py
+++ pages/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
-from .models import Carousel, Feature, Page
+from .models import Carousel, Feature, Page, Message
 
 # Register your models here.
 
 admin.site.register(Carousel)
 admin.site.register(Feature)
+admin.site.register(Message)
 
 
 @admin.register(Page)
