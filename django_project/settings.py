ad5a88836323031b9bf70e7f5c66b8b2678bdb60
Hamed Mohammadi <hamed@dehongi.com>
2025-03-29 17:25:32 +0330

    templates updated

--- django_project/settings.py
+++ django_project/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # Third-party
-    "bootstrap5",
     "taggit",
     # Local
     "accounts.apps.AccountsConfig",
--- pages/forms.py
+++ pages/forms.py
@@ -6,9 +6,22 @@ class PageForm(forms.ModelForm):
     class Meta:
         model = Page
         fields = ["title", "slug", "intro", "body", "image"]
+        widgets = {
+            "title": forms.TextInput(attrs={"class": "form-control"}),
+            "slug": forms.TextInput(attrs={"class": "form-control"}),
+            "intro": forms.Textarea(attrs={"class": "form-control"}),
+            "body": forms.Textarea(attrs={"class": "form-control"}),
+            "image": forms.FileInput(attrs={"class": "form-control"}),
+        }
 
 
 class MessageForm(forms.ModelForm):
     class Meta:
         model = Message
         fields = ["name", "email", "subject", "message"]
+        widgets = {
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            "email": forms.EmailInput(attrs={"class": "form-control"}),
+            "subject": forms.TextInput(attrs={"class": "form-control"}),
+            "message": forms.Textarea(attrs={"class": "form-control"}),
+        }
